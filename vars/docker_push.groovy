8320e50f94520110110ce6f1d6902617684c29ba
NishantPatil240 <143783943+NishantPatil240@users.noreply.github.com>
2026-03-01 16:23:01 +0530

    Fix typo in withCredentials function call

--- vars/docker_push.groovy
+++ vars/docker_push.groovy
@@ -1,5 +1,5 @@
 def call(String project_name, String image_tag, String dockerhub_user){
-  withCrendentials([usernamePassword("credentialsId":"dockerHubCred",usernameVariable:"dockerHubUser",passwordVariable:"dockerHubPass")]){
+  withCredentials([usernamePassword("credentialsId":"dockerHubCred",usernameVariable:"dockerHubUser",passwordVariable:"dockerHubPass")]){
     sh "echo $dockerHubPass | docker login -u $dockerHubUser --pasword-stdin"
   }
   sh "docker push ${dockerhub_user}/${project_name}:${image_tag}"
