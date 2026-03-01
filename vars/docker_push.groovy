79490b7607223053b9d891ecd1e709f2ce3df046
NishantPatil240 <143783943+NishantPatil240@users.noreply.github.com>
2026-03-01 16:24:06 +0530

    Fix typo in docker login command

--- vars/docker_push.groovy
+++ vars/docker_push.groovy
@@ -1,6 +1,6 @@
 def call(String project_name, String image_tag, String dockerhub_user){
   withCredentials([usernamePassword("credentialsId":"dockerHubCred",usernameVariable:"dockerHubUser",passwordVariable:"dockerHubPass")]){
-    sh "echo $dockerHubPass | docker login -u $dockerHubUser --pasword-stdin"
+    sh "echo $dockerHubPass | docker login -u $dockerHubUser --password-stdin"
   }
   sh "docker push ${dockerhub_user}/${project_name}:${image_tag}"
 }
